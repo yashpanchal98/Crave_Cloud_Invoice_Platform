a0593c7ece1a4d5c89c47a1786d5e4bf9a6c5904
yashpanchal98 <pclyash@gmail.com>
2026-02-06 17:02:54 +0530

    axios error fixed

--- frontend/src/apis/api.js
+++ frontend/src/apis/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_BACKEND_URL;
+const API_URL = `${import.meta.env.VITE_BACKEND_URL}/api`;
+
 
 const api = axios.create({
   baseURL: API_URL,
